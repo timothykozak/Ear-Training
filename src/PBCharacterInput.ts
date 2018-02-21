4d3efd0a183d5803f5c337c88e4cad168cd99c96
timothykozak <timothykozak@yahoo.com>
2018-02-21 12:42:30 -0500

    Supports PBConst.

--- src/PBCharacterInput.ts
+++ src/PBCharacterInput.ts
@@ -2,20 +2,23 @@
 // PBCharacterInput.ts
 //
 // Handles the character input from computer keyboard.
+//
+// TODO: Flesh out this stub
 
 import {PBSequencer} from "./PBSequencer.js";
 import {PBTester} from "./PBTester.js";
+import {PBConst} from "./PBConst.js";
 
 class PBCharacterInput {
     theNote: number = 0;
 
     constructor(public sequencer: PBSequencer, public tester: PBTester) {
-        document.addEventListener('keypress', (event: KeyboardEvent) => {this.onCharacterInput(event);}, false)
+        document.addEventListener(PBConst.events.keyPress, (event: KeyboardEvent) => {this.onCharacterInput(event);}, false)
     }
 
     onCharacterInput(event: KeyboardEvent) {
         if (event.key)
-            this.tester.startTest(this.theNote++);
+            this.tester.pickNextNoteToTest();
     }
 }
 
