cf5bc957f56e7f530dbf1404323624b2a0b88aed
timothykozak <timothykozak@yahoo.com>
2018-03-04 14:15:52 -0500

    Canvas, the menu bar and the transport bar are done through innerHTML.

--- src/PBUI.ts
+++ src/PBUI.ts
@@ -17,9 +17,9 @@ interface ClippingRect {
 
 class PBUI {
     static NOTATION_FRACTION_OF_CANVAS = 0.33;
-    static MENU_WIDTH = 50; // In pixels
-    static PLAYER_HEIGHT = 50;
-    static SCROLL_BAR_WIDTH = 35;
+    static MENU_WIDTH = 50; // In pixels, the menu is on the left
+    static PLAYER_HEIGHT = 50; // The transport is on the bottom
+    static SCROLL_BAR_WIDTH = 35; // Have to assume that it is there
     static RESIZE_PAUSE = 200;  // In milliseconds
 
     canvas: HTMLCanvasElement; // The drawing canvas for both notation and keyboard
@@ -31,9 +31,8 @@ class PBUI {
     pianoClippingRect: ClippingRect;
 
     constructor(public statusWindow: PBStatusWindow, public sequencer: PBSequencer) {
-        this.canvas = document.createElement("canvas");
-        document.body.appendChild(this.canvas);
-        this.canvas.style.cssText = "border-style: dotted; position: absolute;";
+        this.buildBodyHTML();
+        this.canvas = document.getElementById("theCanvas") as HTMLCanvasElement;
         this.context = this.canvas.getContext("2d");
         this.onResizeFinished();    // The initial sizing
 
@@ -41,11 +40,34 @@ class PBUI {
                                     // Wait until the resize of the window has paused for a while.
             clearTimeout(this.resizingTimer);
             this.resizingTimer = setTimeout(() => {
-                this.onResizeFinished();}, 200);
+                this.onResizeFinished();}, PBUI.RESIZE_PAUSE);
         };
     }
 
-    onResizeFinished() {
+    buildBodyHTML() {
+        document.body.innerHTML = `
+        <canvas id="theCanvas" style="border-style: dotted; position: absolute;"></canvas>
+        <div class="transportDiv">
+        <ul class="transportList">
+        <li class="transportListItem"><a class="transportListLink" href="#lessthan">&#xf3cf</a></li>
+        <li class="transportListItem"><a onclick="window.pbEarTraining.tester.startTest();" class="transportListLink" href="#start">&#xf488</a></li>
+        <li class="transportListItem"><a class="transportListLink" href="#pause">&#xf478</a></li>
+        <li class="transportListItem"><a class="transportListLink" href="#greaterthan">&#xf3d1</a></li>
+        </ul>
+        </div>
+
+        <div class="menuDiv">
+        <ul class="menuList">
+        <li><a href="#hamburger">&#xf20d</a></li>
+        <li><a href="#gear">&#xf2f7</a></li>
+        <li><a href="#home">&#xf384</a></li>
+        <li><a href="#graph">&#xf2b5</a></li>
+        <li><a href="#questionmark">&#xf444</a></li>
+        </ul>
+        </div>`;
+    }
+
+    onResizeFinished() { // Called during a resize and in the constructor
         this.canvas.width = window.innerWidth - PBUI.MENU_WIDTH - PBUI.SCROLL_BAR_WIDTH;    // Resize the canvas
         this.canvas.height = window.innerHeight - PBUI.PLAYER_HEIGHT - PBUI.SCROLL_BAR_WIDTH;
         this.canvas.style.left = PBUI.MENU_WIDTH + "px";    // Position the canvas
