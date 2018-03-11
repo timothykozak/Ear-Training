1d6d242185a21ea759d4e5605841f58717342d6a
timothykozak <timothykozak@yahoo.com>
2018-03-10 20:23:32 -0500

    Now sends note off messages.  Cleaned up cadenceSequence.

--- src/PBSequencer.ts
+++ src/PBSequencer.ts
@@ -16,6 +16,10 @@ interface SequenceItem {
 }
 
 class PBSequencer {
+    static I_CHORD = [PBSounds.MIDI_MIDDLE_C, PBSounds.MIDI_MIDDLE_C + 4, PBSounds.MIDI_MIDDLE_C + 7];
+    static IV_CHORD = [PBSounds.MIDI_MIDDLE_C + 5, PBSounds.MIDI_MIDDLE_C + 9, PBSounds.MIDI_MIDDLE_C];
+    static V_CHORD = [PBSounds.MIDI_MIDDLE_C + 7, PBSounds.MIDI_MIDDLE_C - 1, PBSounds.MIDI_MIDDLE_C + 2];
+
     sequence: SequenceItem[] = [];
     ticks = 0;
     sequenceRunning = false;
@@ -55,32 +59,37 @@ class PBSequencer {
         document.dispatchEvent(new CustomEvent(PBConst.EVENTS.sequencerCadenceStarted, {detail: this.noteBeingTested}));
     }
 
-    addNoteToSequence(theNote: number, theState: boolean, theTimeInc = 0, theNoteType: NoteType) { // Tack to end of sequence.  To do a chord, don't increment the time
+    addNoteToSequence(theNote: number, theState: boolean, theTimeInc: number, theNoteType: NoteType) { // Tack to end of sequence.  To do a chord, don't increment the time
         let theTime = (this.sequence.length > 0) ? this.sequence[this.sequence.length - 1].time : 0;    // Get time of last note in sequence
         theTime += theTimeInc;
         this.sequence.push({note: theNote, state: theState, time: theTime, noteType: theNoteType} as SequenceItem);
     }
 
+    addChord(theNotes: number[], theState: boolean, theTimeInc: number, theNoteType: NoteType) {
+        theNotes.forEach((theNote, theIndex) => {
+            let localTimeInc = (theIndex == 0) ? theTimeInc : 0;
+            this.addNoteToSequence(theNote, theState, localTimeInc, theNoteType);
+        })
+    }
+
+    addTerminatedChord(theNotes: number[], theNoteType: NoteType) {
+        this.addChord(theNotes, true, 0, theNoteType);  // Turn the chord on
+        this.addChord(theNotes, false, this.ticksBetweenChords, theNoteType);  // Turn the chord on
+    }
+
     cadenceSequence() {
         this.sequence = []; // Clear out old sequence
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C, true, 0, NoteType.Cadence1);    // I
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C + 4, true, 0, NoteType.Cadence1);
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C + 7, true, 0, NoteType.Cadence1);
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C + 5, true, this.ticksBetweenChords, NoteType.Cadence2); // IV
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C + 9, true, 0, NoteType.Cadence2);
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C, true, 0, NoteType.Cadence2);
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C + 7, true, this.ticksBetweenChords, NoteType.Cadence3); // V
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C + -1, true, 0, NoteType.Cadence3);
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C + 2, true, 0, NoteType.Cadence3);
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C, true, this.ticksBetweenChords, NoteType.Cadence4); // I
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C + 4, true, 0, NoteType.Cadence4);
-        this.addNoteToSequence(PBSounds.MIDI_MIDDLE_C + 7, true, 0, NoteType.Cadence4);
+        this.addTerminatedChord(PBSequencer.I_CHORD, NoteType.Cadence1);
+        this.addTerminatedChord(PBSequencer.IV_CHORD, NoteType.Cadence2);
+        this.addTerminatedChord(PBSequencer.V_CHORD, NoteType.Cadence3);
+        this.addTerminatedChord(PBSequencer.I_CHORD, NoteType.Cadence4);
     }
 
     cadencePlusNote(theNote: number) {
         this.noteBeingTested = theNote;
         this.cadenceSequence();
         this.addNoteToSequence(theNote, true, this.ticksBetweenChords * 4, NoteType.Testing);
+        this.addNoteToSequence(theNote, false, this.ticksBetweenChords, NoteType.Testing);
     }
 }
 export {PBSequencer, NoteType, SequenceItem};
