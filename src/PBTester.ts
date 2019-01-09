0b395b0fb323b1c09eef3b8339170556e33c1044
Tim <timothykozak@yahoo.com>
2019-01-08 19:30:46 -0500

    Added some comments.

--- src/PBTester.ts
+++ src/PBTester.ts
@@ -16,14 +16,15 @@ interface ResultItem {
 }
 
 class PBTester {
-    static DEGREE_MIN = 0;
+    static DEGREE_MIN = 0;  // The degree is the position in the 12 tone musical scale,
+                            // with 0 = C, 1 = C#, 2 = D, ... 11 = B
     static DEGREE_MAX = 11;
     static TEST_I_IV_V = [0, 5, 7];
     static TEST_I_TO_VII = [0, 2, 4, 5, 7, 9, 11];
     static TEST_SHARPED = [1, 3, 6, 8, 10];
     static TEST_ALL = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-    private _degreesToTest: Array<number> = PBTester.TEST_I_IV_V;
+    private _degreesToTest: Array<number>;  // The degrees to be tested
     testRunning: boolean = false;
     degreeBeingTested: number;
     waitingForAnswer: boolean;
@@ -32,6 +33,7 @@ class PBTester {
     constructor(public sequencer: PBSequencer) {
         document.addEventListener(PBConst.EVENTS.sequencerNotePlayed, (event: CustomEvent) => {this.onNotePlayed(event);}, false);
         document.addEventListener(PBConst.EVENTS.sequencerTestNotePlayed, (event: CustomEvent) => {this.onTestNotePlayed(event);}, false);
+        this._degreesToTest = PBTester.TEST_I_IV_V;
     }
 
     onTestNotePlayed(event: CustomEvent) {
@@ -47,8 +49,11 @@ class PBTester {
         }
     }
 
-    set degreesToTest(theDegrees: Array<number>) { // Clean it up before using
-        theDegrees.forEach((item: number, index: number) => {
+    set degreesToTest(theDegrees: Array<number>) {
+        // theDegrees contains all the degrees to test.  The same degree can show up
+        // multiple times or the degree may not show up at all.  The order that the
+        // degrees appear does not equal the order that they are played.
+        theDegrees.forEach((item: number, index: number) => { // Remove all invalid degrees
             if ((item > PBTester.DEGREE_MAX) || (item < PBTester.DEGREE_MIN))
                 theDegrees.splice(index, 1); // Remove invalid degrees
         });
@@ -63,7 +68,10 @@ class PBTester {
     }
 
     pickNextNoteToTest(): number {
-        let theResult = -1; // Returns the degree being tested, or -1 for failure.
+        // Used to play the next note in the test.
+        // Picks a note at random from theDegrees and starts the sequence.
+        // Returns the degree being tested, or -1 for failure.
+        let theResult = -1;
         if (!this.sequencer.sequenceRunning && this.testRunning) {  // Still running the test
             let length = this._degreesToTest.length;
             if (length > 0) {
