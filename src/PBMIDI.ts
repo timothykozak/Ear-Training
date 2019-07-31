06d5d05f73af38552ec0dd81dfd824addf99ebc3
Tim <timothykozak@yahoo.com>
2019-07-31 13:01:38 -0400

    Preparation for playing MIDI notes.

--- src/PBMIDI.ts
+++ src/PBMIDI.ts
@@ -12,26 +12,39 @@ import {PBSequencer} from "./PBSequencer";
 import MIDIAccess = WebMidi.MIDIAccess;
 
 class PBMIDI {
-    inputs: WebMidi.MIDIInputMap;
-    outputs: WebMidi.MIDIOutputMap
+    available: boolean = false;
+    inputs: WebMidi.MIDIInputMap = undefined;
+    outputs: WebMidi.MIDIOutputMap = undefined;
 
     constructor(public statusWindow: PBStatusWindow, public sequencer: PBSequencer, public tester: PBTester) {
+        this.available = this.checkForMIDI();
+        document.addEventListener(PBConst.EVENTS.sequencerNotePlayed, (event: CustomEvent) => {this.onSequencer(event)}, false);
+    }
+
+    checkForMIDI(): boolean {
+        let gotMIDI = false;
         if (navigator.requestMIDIAccess) {
-            navigator.requestMIDIAccess().
+            navigator.requestMIDIAccess({sysex: true}).
             then((midiAccess) => {
+                gotMIDI = true;
                 this.statusWindow.writeMsg("MIDI is available.");
                 this.inputs = midiAccess.inputs;
                 this.outputs = midiAccess.outputs;
 
                 for (let input of midiAccess.inputs.values()) {
-                    input.onmidimessage = (message) => {this.handleMIDIMessage(message)};
+                    input.onmidimessage = (message) => {this.onMIDIMessage(message)};
+                }
+                for (let output of midiAccess.outputs.values()) {
+                    output = output;    // Temporary
                 }
             }).catch((error) => {
                 this.statusWindow.writeErr("MIDI is NOT available.");
             });
         }
+        return(gotMIDI);
     }
-    handleMIDIMessage(message: WebMidi.MIDIMessageEvent) {
+
+    onMIDIMessage(message: WebMidi.MIDIMessageEvent) {
         let command = message.data[0];
         let note = message.data[1];
         let velocity = (message.data.length > 2) ? message.data[2] : 0; // a velocity value might not be included with a noteOff command
@@ -58,6 +71,12 @@ class PBMIDI {
     noteOffReceived(note: number) : void {
     }
 
+    onSequencer(event: CustomEvent) {
+        // The sequencer has asked that a note be played.
+        if (this.available && event.detail.state) {
+            // Temporary
+        }
+    }
 }
 
 export {PBMIDI};
